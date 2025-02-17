b821d1347f34b77f544bcda42a1c27c836689b56
ArjavTripathi <arjavtri@iastate.edu>
2025-02-16 23:49:59 -0600

    Hello World

---
+++ src/MyComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MyComponent() {
+    return (
+        <div>
+            <h2>Welcome to My Component!</h2>
+            <p>This is a simple React component created using Vite.</p>
+        </div>
+    );
+}
+
+export default MyComponent;
